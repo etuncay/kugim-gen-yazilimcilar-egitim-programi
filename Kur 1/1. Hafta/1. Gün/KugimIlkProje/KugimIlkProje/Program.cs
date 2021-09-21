94da2d51060b2df554a7ea451d498934ca8438c2
etuncay <etuncay@subu.edu.tr>
2021-09-21 10:34:03 +0300

    ilk gün yapılanlar

---
+++ Kur 1/1. Hafta/1. Gün/KugimIlkProje/KugimIlkProje/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace KugimIlkProje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+           Console.WriteLine("Hello World!");
+        }
+    }
+}
