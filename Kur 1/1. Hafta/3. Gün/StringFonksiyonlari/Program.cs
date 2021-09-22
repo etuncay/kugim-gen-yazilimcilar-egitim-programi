717595400ab90c2a21641e5ab8024a355a729541
etuncay <etuncay@subu.edu.tr>
2021-09-22 20:38:32 +0300

    3 gün kod ve sunu

---
+++ Kur 1/1. Hafta/3. Gün/StringFonksiyonlari/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace StringFonksiyonlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ad = "Ahmet Ali";
+            string ad2 = "";
+            string soyad = " Kaya ";
+
+            var kayaBosluk = soyad.Trim();
+
+            string adSoyad1 = ad + " " + soyad;
+            string adsoyad2 = string.Concat(ad, " ", soyad);
+
+            int adSoyadUzunluk = adsoyad2.Length;
+            var metNerede = adsoyad2.IndexOf("met");
+            var metNeredeTers = adsoyad2.LastIndexOf("met");
+
+            var bastaVarmi = adsoyad2.StartsWith("Ahmet");
+            var bastaVarmi2 = adsoyad2.StartsWith("Ahmet1");
+
+            var sondaVarmi = adsoyad2.EndsWith("Kaya");
+
+            var adSoyadBuyuk = adsoyad2.ToUpper();
+            var adSoyadKucuk = adsoyad2.ToLower();
+
+            var adDegisti = ad.Replace("Ali","Mehmet");
+
+            var adBirkismi = ad.Substring(3,3);
+
+            var adYazilmismi = string.IsNullOrEmpty(ad2);
+
+            if (ad2==null || ad2=="")
+            {
+                var mesaj = "Adınızı giriniz";
+            }
+
+            var kacKelime = ad.Split(' ');
+
+            var isimler = "Ahmet, Ali, Mehmet, Kerim";
+
+            var kacIsimVar = isimler.Split(' ');
+
+            for (int i = 0; i < kacIsimVar.Length; i++)
+            {
+                kacIsimVar[i] = kacIsimVar[i].Replace(",", "");
+            }
+
+            var adDevam = ad.Insert(5, " Mehmet ");
+
+            var temizle = adDevam.Remove(5, 3);
+
+            var sayi1 = 5.3;
+
+            var sayiMutlak = Math.Abs(sayi1);
+
+            var celling = Math.Ceiling(sayi1);
+            var celling2 = Math.Ceiling(5.9);
+
+            var floor = Math.Floor(sayi1);
+            var floor2 = Math.Floor(5.9);
+
+            var round = Math.Round(sayi1);
+            var round2 = Math.Round(5.6);
+            var round3 = Math.Round(5.6561,2);
+
+            var sqrtSayi = Math.Sqrt(3);
+            var powSayi = Math.Pow(3,2);
+
+            var maxSayi = Math.Max(15, 9);
+            var minSayi = Math.Min(15, 9);
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Kur 1/1. Hafta/3. Gün/Uygulama3/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Uygulama3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kisiAdi = "Mehmet ";
+
+
+            int kisiYasi = 1;
+            int kisiYasi1 = 1;
+            string kisiYasi2String = "15";
+
+            
+
+            var toplamKisiYasi = 5;
+
+            toplamKisiYasi = toplamKisiYasi + kisiYasi--;
+
+
+            if ((kisiYasi != toplamKisiYasi || kisiYasi1 != toplamKisiYasi) && (kisiYasi1 > 5))
+            {
+
+            }
+
+            kisiYasi1 = kisiYasi1 + 10;
+            kisiYasi1 += 10;
+
+            kisiYasi1 = kisiYasi1 - 10;
+            kisiYasi1 -= 10;
+            var sonuc = kisiYasi == 5 ? true : false;
+
+            if (kisiYasi == 5)
+            {
+                sonuc = true;
+            }
+            else
+            {
+                sonuc = false;
+            }
+
+            
+
+
+            double? rakam = 1.1;
+
+            var harf = "a";
+            char harf2 = 'a';
+
+            bool? deger = true;
+
+            var modsayisi = 125 % 3;
+
+
+            List<int> IntDizi2 = new List<int>();
+
+            IntDizi2.Add(10);
+
+            int[] IntDizi = new int[10];
+            IntDizi[0] = 10;
+
+
+            List<string> stringDizi1 = new List<string>();
+
+            stringDizi1.Add("Emrullah");
+            stringDizi1.Add("Mehmet");
+            stringDizi1.Add("Ahmet");
+
+            string[] stringDizi2 = new string[10];
+            stringDizi2[0] = "Emrullah";
+            stringDizi2[1] = "Mehmet";
+            stringDizi2[2] = "Ahmet";
+
+            bool[] boolDizi = new bool[10];
+
+            boolDizi[0] = true;
+            boolDizi[11] = true;
+
+
+
+
+
+
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
