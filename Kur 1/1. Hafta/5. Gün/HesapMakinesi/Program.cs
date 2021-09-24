ad0090e636b89d3da347b389d1d4498ffe8168c5
etuncay <etuncay@subu.edu.tr>
2021-09-24 20:36:05 +0300

    5. gün Kişi defteri ve hesap makinesi

---
+++ Kur 1/1. Hafta/5. Gün/HesapMakinesi/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var islemdefteri = new List<string>();
+            var cikis = true;
+            var islem = "";
+            while (cikis)
+            {
+                Menu();
+                Console.WriteLine("İşlem yapmak için rakam giriniz");
+                var menuRakamiString = Console.ReadLine();
+                try
+                {
+                    var menuRakamInt = int.Parse(menuRakamiString);
+                    switch (menuRakamInt)
+                    {
+                        case 1:
+                            var toplamIslemi = true;
+                            var toplam = 0;
+                            var sayiSira = 1;
+                            var islemSayi = "";
+                            while (toplamIslemi)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Lütfen bir sayı giriniz");
+                                var sayiString = Console.ReadLine();
+                                
+                                if (int.TryParse(sayiString , out int sayiInt))
+                                {
+                                    toplam = toplam + sayiInt;
+                                    islemSayi = islemSayi+ " sayi " + sayiSira + " =" + sayiInt+" ,";
+                                    sayiSira++;
+                                }
+                                else
+                                {
+                                    toplamIslemi = false;
+                                }
+                            }
+                            islem = "Yapılan işlem toplama " + islemSayi +" toplamı = "+ toplam;
+
+                            Console.WriteLine("Toplam = " + toplam);
+
+                            //Console.Write("1. sayıyı giriniz = ");
+                            //var sayi1String1 = Console.ReadLine();
+                            //Console.Write("2. sayıyı giriniz = ");
+                            //var sayi2String1 = Console.ReadLine();
+
+                            //var sayi1Int1 = int.Parse(sayi1String1);
+                            //var sayi2Int1 = int.Parse(sayi2String1);
+
+                            //var toplam = sayi1Int1 + sayi2Int1;
+                            //Console.WriteLine("2 sayının toplamı =" + toplam);
+                            //islem = "Yapılan işlem toplama sayi 1=" + sayi1Int1+", sayi 2 = " + sayi2Int1+ " toplamı = "+ toplam;
+
+
+                            islemdefteri.Add(islem);
+                            Bekleme();
+                            Console.Clear();
+                            break;
+                        case 2:
+                            Console.Write("1. sayıyı giriniz = ");
+                            var sayi1String2 = Console.ReadLine();
+                            Console.Write("2. sayıyı giriniz = ");
+                            var sayi2String2 = Console.ReadLine();
+
+                            var sayi1Int2 = int.Parse(sayi1String2);
+                            var sayi2Int2 = int.Parse(sayi2String2);
+                            var cikan = sayi1Int2 - sayi2Int2;
+                            Console.WriteLine("2 sayının çıkmarması =" + cikan);
+                            islem = "Yapılan işlem çıkmarma sayi 1=" + sayi1Int2 + ", sayi 2 = " + sayi2Int2 + " cıkan = " + cikan;
+                            islemdefteri.Add(islem);
+                            Bekleme();
+                            Console.Clear();
+                            break;
+                        case 3:
+                            Console.Write("1. sayıyı giriniz = ");
+                            var sayi1String3 = Console.ReadLine();
+                            Console.Write("2. sayıyı giriniz = ");
+                            var sayi2String3 = Console.ReadLine();
+
+                            var sayi1Int3 = int.Parse(sayi1String3);
+                            var sayi2Int3 = int.Parse(sayi2String3);
+                            var carpma = sayi1Int3 * sayi2Int3;
+                            Console.WriteLine("2 sayının çarpımı = "+ carpma);
+                            islem = "Yapılan işlem çarpma sayi 1=" + sayi1Int3 + ", sayi 2 = " + sayi2Int3 + " çarpım = " + carpma;
+
+                            islemdefteri.Add(islem);
+                            Bekleme();
+                            Console.Clear();
+                            break;
+                        case 4:
+                            Console.Write("1. sayıyı giriniz = ");
+                            var sayi1String4 = Console.ReadLine();
+                            Console.Write("2. sayıyı giriniz = ");
+                            var sayi2String4 = Console.ReadLine();
+
+                            var sayi1Int4 = int.Parse(sayi1String4);
+                            var sayi2Int4 = int.Parse(sayi2String4);
+
+                            var bolmesi = sayi1Int4 / sayi2Int4;
+                            Console.WriteLine("2 sayının bölümü = " + bolmesi);
+                            islem = "Yapılan işlem bölme sayi 1=" + sayi1Int4 + ", sayi 2 = " + sayi2Int4 + " bölüm = " + bolmesi;
+                            islemdefteri.Add(islem);
+                            Bekleme();
+                            Console.Clear();
+                            break;
+                        case 5:
+                            var x = 0;
+                            foreach (var kayit in islemdefteri)
+                            {
+                                x++;
+                                Console.WriteLine(x + " : " + kayit);
+                            }  
+
+                            Bekleme();
+                            Console.Clear();
+                            break;
+                        case 6: break;
+                        default:
+                            MenuRakamKontrol();
+                            break;
+                    }
+                }
+                catch
+                {
+                    MenuRakamKontrol();
+                }
+            }
+
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("1 - Toplama");
+            Console.WriteLine("2 - Çıkarma");
+            Console.WriteLine("3 - Çarpma");
+            Console.WriteLine("4 - Bölme");
+            Console.WriteLine("5 - Son Yapılan İşlemler");
+            Console.WriteLine("6 - Çıkış");
+        }
+
+        public static void Bekleme()
+        {
+            Console.WriteLine("Menüye geçmek için herhangi bir tuşa basınız");
+            Console.ReadLine();
+        }
+
+        public static void MenuRakamKontrol()
+        {
+            Console.Clear();
+            Console.WriteLine("Lütfen 1 ile 6 arası bir seçim yapınız. Menüye geçmek için herhangi bir tuşa basınız.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
---
+++ Kur 1/1. Hafta/5. Gün/KisiDefteri/Program.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace KisiDefteri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Kişi Bilgileri
+            // Kişi Bilgileri Listele
+            // Kişi Bilgisi Ekle
+            // Kişi Bilgisi Güncelle
+            // Kişi Bilgisi Sil
+            
+            var defter = new string[100,2];
+            var cikis = true;
+            var i = 0;
+            while (cikis)
+            {
+                Menu();
+                Console.WriteLine("İşlem yapmak için rakam giriniz");
+                var menuRakamiString = Console.ReadLine();
+                try
+                {
+                    var menurakamiInt = int.Parse(menuRakamiString);
+
+                    Console.Clear();
+                    switch (menurakamiInt)
+                    {
+                        case 1:
+                            Console.WriteLine("###### Kişi Listesi ###### ");
+                            for (int k = 0; k < (defter.Length / 2); k++)
+                            {
+                                if (!string.IsNullOrEmpty(defter[k, 0]) || !string.IsNullOrEmpty(defter[k, 1]))
+                                {
+                                    Console.WriteLine((k + 1) + ": Ad Soyad = " + defter[k, 0] + " " + defter[k, 1]);
+                                }
+                            }
+                            Bekleme();
+                            Console.Clear();
+                            break;
+                        case 2:
+                            Console.WriteLine("###### Kişi Ekle ###### ");
+                            Console.Write("Ad Bilgisini Giriniz = ");
+                            string ad = Console.ReadLine();
+                            Console.Write("Soyad Bilgisini Giriniz = ");
+                            string soyad = Console.ReadLine();
+                            defter[i, 0] = ad;
+                            defter[i, 1] = soyad;
+                            i++;
+                            Bekleme();
+                            Console.Clear();
+                            break;
+                        case 3:
+                            Console.WriteLine("###### Kişi Güncelle ###### ");
+                            Console.WriteLine("Güncellemek istediğiniz kullanıcının numarasını giriniz");
+                            var numaraString = Console.ReadLine();
+                            var numaraInt = int.Parse(numaraString)-1;
+                            var adS = defter[numaraInt, 0];
+                            var soyadS = defter[numaraInt, 1];
+                            if (!string.IsNullOrEmpty(adS) || !string.IsNullOrEmpty(soyadS))
+                            {
+                                Console.WriteLine("Bulunan Ad Soyad =" + adS + " "+ soyadS);
+                                Console.Write("Ad Bilgisini Giriniz = ");
+                                string adS2 = Console.ReadLine();
+                                Console.Write("Soyad Bilgisini Giriniz = ");
+                                string soyadS2 = Console.ReadLine();
+                                if (!string.IsNullOrEmpty(adS2))
+                                {
+                                    defter[numaraInt, 0] = adS2;
+                                }
+                                if (!string.IsNullOrEmpty(soyadS2))
+                                {
+                                    defter[numaraInt, 1] = soyadS2;
+                                }
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Aradığınız Kişi Bulunamadı");                                
+                            }
+
+                            Bekleme();
+                            Console.Clear();
+                            break;
+                        case 4:
+                            Console.WriteLine("###### Kişi Sil ###### ");
+                            Console.WriteLine("Silmek istediğiniz kişinin numarasını giriniz");
+                            var numaraString1 = Console.ReadLine();
+                            var numaraInt1 = int.Parse(numaraString1)-1;
+                            var adS3 = defter[numaraInt1, 0];
+                            var soyadS3 = defter[numaraInt1, 1];
+                            Console.WriteLine("Silmek istediğiniz Ad Soyad = '"+ adS3+ " "+ soyadS3+ "' buysa E Tuşuna basınız.Değilse H tuşuna basınız.");
+                            var islem = Console.ReadLine();
+                            //if(islem=="E" || islem == "e")
+                            //if(islem.ToLower()=="e")
+                            if (islem.ToUpper() == "E")
+                            {
+                                defter[numaraInt1, 0] = null;
+                                defter[numaraInt1, 1] = null;
+                                Console.Write("Silme işlemi tamamlandı.");
+                            }
+                            Bekleme();
+                            Console.Clear();
+                            break;
+                        case 5:
+                            Console.WriteLine("Çıkış Yaptınız.");
+                            Console.Clear();
+                            cikis = false;
+                            break;
+                        default:
+                            MenuRakamKontrol();
+                            break;
+                    }
+                }
+                catch
+                {
+
+                    MenuRakamKontrol();
+                }
+                
+                
+            }
+                     
+        }
+
+        public static void Menu()
+        {
+            Console.WriteLine("1 - Listele");
+            Console.WriteLine("2 - Ekle");
+            Console.WriteLine("3 - Güncelle");
+            Console.WriteLine("4 - Sil");
+            Console.WriteLine("5 - Çıkış");
+        } 
+
+        public static void Bekleme()
+        {
+            Console.WriteLine("Menüye geçmek için herhangi bir tuşa basınız");
+            Console.ReadLine();
+        }
+
+        public static void MenuRakamKontrol()
+        {
+            Console.Clear();
+            Console.WriteLine("Lütfen 1 ile 5 arası bir seçim yapınız. Menüye geçmek için herhangi bir tuşa basınız.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
