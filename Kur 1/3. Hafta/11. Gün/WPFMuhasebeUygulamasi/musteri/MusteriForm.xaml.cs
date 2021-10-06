f34e5c49e5db8c5c071848c376bd10785a3f58b9
Emrullah TUNCAY <etuncay@subu.edu.tr>
2021-10-06 20:07:06 +0300

    11. Gün WPF uygulaması başlangıç

---
+++ Kur 1/3. Hafta/11. Gün/WPFMuhasebeUygulamasi/musteri/MusteriForm.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFMuhasebeUygulamasi.musteri
+{
+    /// <summary>
+    /// Interaction logic for MusteriForm.xaml
+    /// </summary>
+    public partial class MusteriForm : Window
+    {
+        public MusteriForm()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var musteriListe = new MusteriListe();
+            musteriListe.Show();
+            this.Close();
+        }
+    }
+}
