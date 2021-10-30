5de4cea0292c7a2887775758c5db2e920f33b273
etuncay <etuncay@subu.edu.tr>
2021-10-30 21:42:16 +0300

    activasyon silindi

--- LMS/Migrations/20211030182133_KullaniciEntity-Aktivasyon-silindi.Designer.cs
+++ LMS/Migrations/20211030182133_KullaniciEntity-Aktivasyon-silindi.Designer.cs
@@ -4,14 +4,16 @@ using LMS.Models.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LMS.Migrations
 {
     [DbContext(typeof(LMSDbContext))]
-    partial class LMSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211030182133_KullaniciEntity-Aktivasyon-silindi")]
+    partial class KullaniciEntityAktivasyonsilindi
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LMS/Migrations/20211030182133_KullaniciEntity-Aktivasyon-silindi.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LMS.Migrations
+{
+    public partial class KullaniciEntityAktivasyonsilindi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AktivasyonKey",
+                table: "Kullanici");
+
+            migrationBuilder.DropColumn(
+                name: "AktivasyonSonTarihSaat",
+                table: "Kullanici");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AktivasyonKey",
+                table: "Kullanici",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AktivasyonSonTarihSaat",
+                table: "Kullanici",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
