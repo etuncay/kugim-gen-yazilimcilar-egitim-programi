fc0a5572a8ebac58d8b1fcc2f44230d443ecb9a2
etuncay <etuncay@subu.edu.tr>
2021-11-21 21:25:03 +0300

    componentler oluşturuldu

---
+++ Haber/Haber.MVC/ViewComponents/KategoriHaberViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using Haber.Models.ViewModels;
+using Haber.Models.ViewModels.Response;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Haber.MVC.ViewComponents
+{
+    public class KategoriHaberViewComponent : ViewComponent
+    {
+        private readonly RestClient _restClient;
+
+        public KategoriHaberViewComponent()
+        {
+            _restClient = new RestClient("http://localhost:42939/api/");
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync(int kategoriId, int al)
+        {
+
+            var request = new RestRequest($"/Icerik/Filtrele?Sayfalama.Al={al}&Sayfalama.Sayfalama=true&KategoriId={kategoriId}");
+            var response = await _restClient.GetAsync<ResponseResultModel<List<IcerikResponseViewModel>>>(request);
+
+            var result = response.Data;
+
+            return View(result);
+        }
+    }
+}
---
+++ Haber/Haber.MVC/ViewComponents/MansetHaberViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Haber.Models.ViewModels;
+using Haber.Models.ViewModels.Response;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Haber.MVC.ViewComponents
+{
+    public class MansetHaberViewComponent : ViewComponent
+    {
+        private readonly RestClient _restClient;
+
+        public MansetHaberViewComponent()
+        {
+            _restClient = new RestClient("http://localhost:42939/api/");
+        }
+
+
+        public async Task<IViewComponentResult> InvokeAsync(int al)
+        {
+
+            var request = new RestRequest($"Icerik/Listele?al={al}&atla=0&sayfala=true");
+
+            var response = await _restClient.GetAsync<ResponseResultModel<List<IcerikResponseViewModel>>>(request);
+            var result = response.Data;
+            return View(result);
+        }
+    }
+}
